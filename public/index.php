5032c8dd025d22af90d5aefbea0538bb5bec2f7c
Eric Nguyen <enguyen807@gmail.com>
2023-07-13 01:20:09 +0300

    Update router

--- public/index.php
+++ public/index.php
@@ -8,4 +8,11 @@ spl_autoload_register(function ($class) {
     require base_path("{$class}.php");
 });
 
-require base_path("router/router.php");
+$router = new \Router\Router();
+
+$routes = require base_path('router/routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
--- router/router.php
+++ router/router.php
@@ -1,14 +1,52 @@
 <?php
 
-$routes = require base_path('router/routes.php');
+namespace Router;
 
-function routeToController($uri, $routes) {
-    if (array_key_exists($uri, $routes)) {
-        require base_path($routes[$uri]);
-    } else {
+class Router {
+    protected $routes = [];
+
+    public function route($uri, $method)
+    {
+        foreach($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                return require base_path($route['controller']);
+            }
+        }
+
         abort();
     }
-}
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-routeToController($uri, $routes);
+    public function add($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method
+        ];
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->add('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->add('POST', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->add('DELETE', $uri, $controller);
+    }
+
+    public function patch($uri, $controller)
+    {
+        $this->add('PATCH', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        $this->add('PUT', $uri, $controller);
+    }
+}
