3e504a24a899c784bbba7448da4676ad97fac819
Eric Nguyen <enguyen807@gmail.com>
2023-07-18 02:26:51 +0300

    Add delete button

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,7 +10,12 @@ require base_path("views/partials/banner.php");
         </p>
         <h1><?= htmlspecialchars($note['title']) ?></h1>
 
-        <footer class="mt-6">
+        <footer class="flex gap-x-6 mt-6">
+            <form method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+            </form>
             <a href="/note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-emerald-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">Edit</a>
         </footer>
     </div>
