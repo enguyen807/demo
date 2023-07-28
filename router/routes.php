e8806d4d3011d80b5640b516c07989255a18292e
Eric Nguyen <enguyen807@gmail.com>
2023-07-29 02:15:24 +0300

    Refactor

--- router/routes.php
+++ router/routes.php
@@ -1,23 +1,23 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
+$router->get('/', 'index.php');
+$router->get('/about', 'about.php');
+$router->get('/contact', 'contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php')->middleware('auth');
-$router->get('/note', 'controllers/notes/show.php');
-$router->delete('/note', 'controllers/notes/destroy.php');
+$router->get('/notes', 'notes/index.php')->middleware('auth');
+$router->get('/note', 'notes/show.php');
+$router->delete('/note', 'notes/destroy.php');
 
-$router->get('/note/edit', 'controllers/notes/edit.php');
-$router->patch('/note', 'controllers/notes/update.php');
+$router->get('/note/edit', 'notes/edit.php');
+$router->patch('/note', 'notes/update.php');
 
 
-$router->get('/notes/create', 'controllers/notes/create.php');
-$router->post('/notes', 'controllers/notes/store.php');
+$router->get('/notes/create', 'notes/create.php');
+$router->post('/notes', 'notes/store.php');
 
-$router->get('/register', 'controllers/registration/create.php')->middleware('guest');
-$router->post('/register', 'controllers/registration/store.php')->middleware('guest');
+$router->get('/register', 'registration/create.php')->middleware('guest');
+$router->post('/register', 'registration/store.php')->middleware('guest');
 
-$router->get('/login', 'controllers/sessions/create.php')->middleware('guest');
-$router->post('/login', 'controllers/sessions/store.php')->middleware('guest');
-$router->delete('/logout', 'controllers/sessions/destroy.php')->middleware('auth');
+$router->get('/login', 'sessions/create.php')->middleware('guest');
+$router->post('/login', 'sessions/store.php')->middleware('guest');
+$router->delete('/logout', 'sessions/destroy.php')->middleware('auth');
